e81f9a87c7ddf9da7c44f9edbc0a5d7cff27ee7a
Scott Sanbar <scott.sanbar@gmail.com>
2012-04-07 17:42:05 -0500

    Initial Commit

---
+++ src/MDNGuideExercises001.js
@@ -0,0 +1,7 @@
+// Portions copyright 2012 Scott Sanbar.  See COPYRIGHT file for details
+// Original Author (portions):  Scott Sanbar - scott.sanbar@gmail.com
+//
+// MDNGuideExercises001.js - Miscellaneous exercises as I work through the
+// Mozilla Developer Network Guide - some exercises are from the MDN Guide.
+
+
