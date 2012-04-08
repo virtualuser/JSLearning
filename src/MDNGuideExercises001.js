df8c3f175f2a65a5de3fee6424222d3047ed9ad7
Scott Sanbar <scott.sanbar@gmail.com>
2012-04-07 20:49:47 -0500

    Got dynamic evaluation and print to web page working

--- src/MDNGuideExercises001.js
+++ src/MDNGuideExercises001.js
@@ -4,4 +4,22 @@
 // MDNGuideExercises001.js - Miscellaneous exercises as I work through the
 // Mozilla Developer Network Guide - some exercises are from the MDN Guide.
 
+function printAndEvalExpression(expression, printEval, id) {
 
+    if (printEval) {
+        document.getElementById(id).innerHTML += expression + " = " + eval(expression)
+            + " | evaluated |<br />";
+    } else {
+        document.getElementById(id).innerHTML += expression + "<br />";
+        eval(expression);
+    }
+}
+
+function runExercises(id) {
+    document.getElementById(id).innerHTML = "";
+    printAndEvalExpression("1 + 1", true, id);
+    printAndEvalExpression("answer = 42", false, id);
+    printAndEvalExpression("answer", true, id);
+    printAndEvalExpression("answer = 'Hello, answer!'", false, id);
+    printAndEvalExpression("answer", true, id);
+}
