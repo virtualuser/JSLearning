d898697311953c59335d8cfffe866bd3d7fe1c80
Scott Sanbar <scott.sanbar@gmail.com>
2012-04-07 22:01:30 -0500

    Improved greatly - using object array for parameters to evaluation and print function.

--- src/MDNGuideExercises001.js
+++ src/MDNGuideExercises001.js
@@ -4,22 +4,38 @@
 // MDNGuideExercises001.js - Miscellaneous exercises as I work through the
 // Mozilla Developer Network Guide - some exercises are from the MDN Guide.
 
-function printAndEvalExpression(expression, printEval, id) {
+function gJSL_params(expression, id, printEval) {
+    this.expression = expression;
+    this.id = id;
+    this.printEval = printEval;
+}
 
-    if (printEval) {
-        document.getElementById(id).innerHTML += expression + " = " + eval(expression)
-            + " | evaluated |<br />";
-    } else {
-        document.getElementById(id).innerHTML += expression + "<br />";
-        eval(expression);
+function gJSL_printAndEvalExpressions(paramsArray) {
+    
+    for (var index in paramsArray) {
+
+        if (paramsArray[index].printEval) {
+            document.getElementById(paramsArray[index].id).innerHTML
+                += paramsArray[index].expression + " = " + eval(paramsArray[index].expression)
+                + " | evaluated |<br />";
+        } else {
+            document.getElementById(paramsArray[index].id).innerHTML
+                += paramsArray[index].expression + " | expression |<br />";
+            eval(paramsArray[index].expression);
+        }
     }
 }
 
 function runExercises(id) {
     document.getElementById(id).innerHTML = "";
-    printAndEvalExpression("1 + 1", true, id);
-    printAndEvalExpression("answer = 42", false, id);
-    printAndEvalExpression("answer", true, id);
-    printAndEvalExpression("answer = 'Hello, answer!'", false, id);
-    printAndEvalExpression("answer", true, id);
+    
+    var paramsArray = [
+        new gJSL_params("1 + 1", id, true),
+        new gJSL_params("var answer = 42;", id, false),
+        new gJSL_params("answer", id, true),
+        new gJSL_params("answer = 'Hello, answer!';", id, false),
+        new gJSL_params("answer", id, true)
+    ];
+    
+    gJSL_printAndEvalExpressions(paramsArray);
 }
