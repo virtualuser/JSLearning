cd15c231a319d45ee085ec17f045737efdd9dec1
Scott Sanbar <scott.sanbar@gmail.com>
2012-04-11 00:22:09 -0500

    Revamped almost completely.

---
+++ src/MDNGuideExercises002.js
@@ -0,0 +1,15 @@
+// Portions copyright 2012 Scott Sanbar.  See COPYRIGHT file for details
+// Original Author (portions):  Scott Sanbar - scott.sanbar@gmail.com
+//
+// MDNGuideExercises001.js - Miscellaneous exercises as I work through the
+// Mozilla Developer Network Guide - some exercises are from the MDN Guide.
+
+function gJSL_displayInput(idInput, idOutput) {
+
+    var ifrm = document.getElementById(idOutput);
+    var cnt = (ifrm.contentWindow || ifrm.contentDocument);
+    var doc = cnt.document;
+    doc.open();
+    doc.write(document.getElementById(idInput).value);
+    doc.close();
+}
