49904d54839ae1dd7ec42004bdf4bb3d3abfe3cf
Scott Sanbar <scott.sanbar@gmail.com>
2012-06-16 21:26:09 -0500

    src/Extend.js

--- src/Extend.js
+++ src/Extend.js
@@ -1,3 +1,31 @@
+// No known Copyright
+// Original Author:  FireFly - irc.freenode.net - Jonas Höglund see THANKS File
+
+///////////////
+// Library code
+///////////////
+
+var Base = {}
+// Base#extend
+Object.defineProperty(Base, 'extend', {
+  enumerable: false,
+  value: function(obj) {
+    var descs = {}
+
+    ++objectInheritCounter
+
+    Object.getOwnPropertyNames(obj).forEach(function(key) {
+      descs[key] = Object.getOwnPropertyDescriptor(obj, key)
+    })
+
+    return Object.create(this, descs)
+  }
+})
+
+///////////////
+// Sample Usage
+///////////////
+
 var Person = Base.extend({
   // missing: name
 
@@ -18,20 +46,3 @@ var WorkingPerson = Person.extend({
 
 var p1 = WorkingPerson.extend({ name:"Harry", occupation:"wizard" })
 p1.talk() // "Hello, I'm Harry and I am a wizard"
-
-var Base = {}
-// Base#extend
-Object.defineProperty(Base, 'extend', {
-  enumerable: false,
-  value: function(obj) {
-    var descs = {}
-
-    ++objectInheritCounter
-
-    Object.getOwnPropertyNames(obj).forEach(function(key) {
-      descs[key] = Object.getOwnPropertyDescriptor(obj, key)
-    })
-
-    return Object.create(this, descs)
-  }
-})
