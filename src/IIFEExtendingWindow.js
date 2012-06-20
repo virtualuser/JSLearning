c2985ed99d3f1f879e98ba0c6ef32ac93e52f74e
Scott Sanbar <scott.sanbar@gmail.com>
2012-06-20 01:42:12 -0500

    Added examples of coding onLoad functions/object bags

---
+++ src/IIFEExtendingWindow.js
@@ -0,0 +1,21 @@
+(function(global) { // global is == to window
+  "use strict"; // Tells JS engine to warn you about silly things you've done
+
+  var myVariable = 10;
+
+  function internalFunction(val) {
+    return myVarible * val;
+  }
+
+  global.externalFunction = function(num){
+    return internalFunction(num);
+  };
+
+})(window);
+// We pass "window" like that above, because we may want to pass other objects
+// in future. One possibility is "module" if this library will be used for 
+// a node.js library or similar.
+
+// Now, in another file later we can do:
+
+var t = externalFunction(2); // t = 20
---
+++ src/InputFilesBag.js
@@ -0,0 +1,24 @@
+// Copyright 2011-2012 Scott Sanbar.  See COPYRIGHT file for details
+// Original Author:  Scott Sanbar - scott.sanbar@gmail.com
+
+function load() {
+    mySpace = {
+        fileSelect: document.getElementById("fileSelect"),
+        fileElem: document.getElementById("fileElem"),
+        fileNames: document.getElementById("fileNames"),
+        
+        handleFiles: function(files) {
+            files.length > 0 ? this.fileNames.innerHTML = ''
+                : this.fileNames.innerHTML = '(No Files Selected)';
+            for (var i = 0; i < files.length; i++) {
+                this.fileNames.innerHTML += files[i].name + '<br />';
+            }
+        }
+    };
+    fileSelect.addEventListener("click", function (e) {  
+        if (mySpace.fileElem) {  
+            mySpace.fileElem.click();  
+        }  
+        e.preventDefault(); // prevent navigation to "#"  
+    }, false);
+}
