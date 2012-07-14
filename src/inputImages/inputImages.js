df8f7119277a9d624c43bcb5d65d196887bf167d
Scott Sanbar <scott.sanbar@gmail.com>
2012-07-13 19:03:36 -0500

    updated using jslint and corrected error in inputImages

--- src/inputImages/inputImages.js
+++ src/inputImages/inputImages.js
@@ -2,85 +2,91 @@
 // Original Author:  Scott Sanbar - scott.sanbar@gmail.com
 
 window.addEventListener('load', function () {
+    "use strict";
+
     var fileElem = document.getElementById('fileElem'),
         fileSelect = document.getElementById('fileSelect'),
-        message = document.getElementById('message');
+        message = document.getElementById('message'),
 
-    var mySpace = {
-        numImgs: 0,
-        numLoadedImgs: 0,
-        imgs: [],
+        mySpace = {
+            numImgs: 0,
+            numLoadedImgs: 0,
+            imgs: [],
 
-        imgAppend: function () {
-            var i;
-            message.innerHTML = '';
-            message.hidden = true;
-            this.imgs.forEach(function (img) {
-                message.appendChild(img);
-            });
-            message.hidden = false;
-        },
+            imgAppend: function () {
+                var i;
+                message.innerHTML = '';
+                message.hidden = true;
+                this.imgs.forEach(function (img) {
+                    message.appendChild(img);
+                });
+                message.hidden = false;
+            },
 
-        imgLoad: function (img) {
-            this.numLoadedImgs += 1;
-            if (this.numLoadedImgs === this.numImgs) {
-                this.imgAppend();
+            imgLoad: function (img) {
+                this.numLoadedImgs += 1;
+                if (this.numLoadedImgs === this.numImgs) {
+                    this.imgAppend();
+                }
+            },
+
+            readerLoad: function (img) {
+                return function (e) {
+                    img.src = e.target.result;
+                };
+            },
+
+            handleFiles: function (files) {
+                var file,
+                    reader,
+                    imageType = /image.*/,
+                    i;
+
+                this.imgs.forEach(function (img) {
+                    message.removeChild(img);
+                });
+
+                this.imgs = [];
+                this.numLoadedImgs = 0;
+                this.numImgs = files.length;
+
+                files = [].slice.call(files);
+
+                files.forEach(function (file) {
+
+                    var img;
+
+                    if (!file.type.match(imageType)) {
+                        this.numImgs -= 1;
+                        return;
+                    }
+
+                    img = new Image();
+                    this.imgs.push(img);
+                    img.className = "thumb";
+                    img.file = file;
+                    img.addEventListener('load', this.imgLoad(img));
+
+                    reader = new FileReader();
+                    reader.addEventListener('load', this.readerLoad(img));
+                    reader.readAsDataURL(file);
+
+                }.bind(this));
+
+                if (this.numImgs === 0) {
+                    message.innerHTML = '(No Images Selected)';
+                }
             }
-        },    
-
-        readerLoad: function(img) {return function(e) {
-            img.src = e.target.result;};
-        },
-
-        handleFiles: function(files) {
-            var file,
-                reader,
-                imageType = /image.*/,  
-                i;
-
-            this.imgs.forEach(function (img) {
-                message.removeChild(img);
-            });
-
-            this.imgs = [];
-            this.numLoadedImgs = 0;
-            this.numImgs = files.length;
-
-            files.forEach(function(file){ 
-                
-                var img;
-
-                if (!file.type.match(imageType)) {  
-                    this.numImgs -= 1;
-                    return;  
-                }  
-
-                img = new Image();
-                this.imgs.push(img);
-                img.className = "thumb";
-                img.file = file;
-                img.addEventListener('load', this.imgLoad(img));  
-
-                reader = new FileReader();
-                reader.addEventListener('load', this.readerLoad(img));  
-                reader.readAsDataURL(file);  
-
-            }.bind(this));
-
-        if (this.numImgs === 0) {
-                message.innerHTML = '(No Images Selected)';
-            }
-        }
-    };
+        };
 
     fileElem.addEventListener("change", function (e) {
-            mySpace.handleFiles(this.files);
+        mySpace.handleFiles(this.files);
     }, false);
 
-    fileSelect.addEventListener("click", function (e) {  
-        if (fileElem) {  
-            fileElem.click();  
-        }  
+    fileSelect.addEventListener("click", function (e) {
+        if (fileElem) {
+            fileElem.click();
+        }
         e.preventDefault(); // prevent navigation to "#"  
     }, false);
 });
