5b7bf2fc51bfc25b7cbf51ad60f7181535b1ccd5
Scott Sanbar <scott.sanbar@gmail.com>
2013-07-02 22:06:53 -0500

    Working well

--- src/survey/survey.js
+++ src/survey/survey.js
@@ -2,22 +2,36 @@
 // Original Author:  Scott Sanbar - scott.sanbar@gmail.com
 
 function getResults() {
-    "use strict";
+    'use strict';
 
     var i,
         j,
-        radios = [document.getElementsByName("sq1Radios"), document.getElementsByName("sq2Radios")];
+        output = 'Name: ' + document.getElementById('name').value + '\n' +
+            'Email: ' + document.getElementById('email').value + '\n',
+        radios = [document.getElementsByName('sq1Radio'), document.getElementsByName('sq2Radio')];
 
     for (i = 0; i < radios.length; i++) {
         for (j = 0; j < radios[i].length; j++)
         {
             if (radios[i][j].checked) {
-                alert("Name: " + document.getElementById('name').value + "\n" +
-                    "Email: " + document.getElementById('email').value + '\n' +
-                    " Question 1: " + document.getElementById('sq' + i + 'RadioLabel' + j).innerHTML);
+                output += 'Question ' + (i + 1) + ': ' +
+                    document.getElementById('sq' + (i + 1) + 'RadioLabel' + (j + 1)).innerHTML + '\n';
                 break;
             }
         }
     }
+
+    if (output !== '') {
+        alert(output);
+    }
+
     return false;
 }
+
+shim.addEventListener(window, 'load',
+    function () {
+        'use strict';
+
+        shim.addEventListener(document.getElementById('formSurvey'), 'submit', getResults);
+    }
+);
